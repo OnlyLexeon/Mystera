d76cf895a680e5d0ed3ec9539d1eb8a572e312ce
LexPolka <lexthepolka@gmail.com>
2025-08-06 19:13:24 +0800

    Added Nav Mesh to Dungeon Rooms

--- Assets/! Scripts/Dungeons Scripts/DungeonRoom.cs
+++ Assets/! Scripts/Dungeons Scripts/DungeonRoom.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum RoomType
@@ -9,12 +10,30 @@ public enum RoomType
     Empty
 }
 
+public class EnemySetting
+{
+    public float weightage;
+    public GameObject enemyPrefab;
+}
 
 public class DungeonRoom : MonoBehaviour
 {
     public RoomType roomType;
     public List<DungeonConnector> connectors;
     public BoxCollider boundsCollider;
+
+    [Header("Enemy Settings")]
+    public bool hasEnemy = false;
+    public Transform[] enemySpawnPoints;
+    public EnemySetting[] enemyToSpawn;
+
+    private void Start()
+    {
+        foreach (Transform spawnPoint in enemySpawnPoints)
+        {
+
+        }
+    }
 
     public Bounds GetWorldBounds()
     {
