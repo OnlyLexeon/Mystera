2cfe08799e4258d48d5c0ce6a7c41aab54ca5f04
LexPolka <lexthepolka@gmail.com>
2025-08-21 14:35:54 +0800

    fixed tutorials not happening

--- Assets/! Scripts/UI/TutorialButtonChecker.cs
+++ Assets/! Scripts/UI/TutorialButtonChecker.cs
@@ -4,12 +4,34 @@ public class TutorialButtonChecker : MonoBehaviour
 {
     public GameObject tutorialButton;
 
+    private TutorialManager tutorialManager;
+    private bool canDoTutorial = false;
+
     void Start()
     {
         SceneController sceneController = SceneController.instance;
-        if (sceneController.IsMainScene()) tutorialButton.SetActive(true);
+        if (sceneController.IsMainScene()) CanDoTutorial();
         else tutorialButton.SetActive(false);
 
         Debug.Log($"Tutorial Button: {tutorialButton.activeSelf}");
+    }
+
+    public void CanDoTutorial()
+    {
+        canDoTutorial = true;
+
+        tutorialButton.SetActive(true);
+
+        tutorialManager = TutorialManager.instance;
+    }
+
+    public void DoTutorial()
+    {
+        if (canDoTutorial && tutorialManager) tutorialManager.StartTutorial();
+    }
+
+    public void ToggleDoNotShowAgain()
+    {
+        if (canDoTutorial && tutorialManager) tutorialManager.ToggleDoNotShowAgain();
     }
 }
